6ba8335dd47f47a1a4dc52bbcd64eaf7045dd81b
Anubhav Gupta <anubhav.gupta5@mail.dcu.ie>
2020-06-02 01:36:55 +0530

    Time Conversion

---
+++ Time_Conversion.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import os
+import sys
+from datetime import datetime
+
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s):
+    date=datetime.strptime(s, '%I:%M:%S%p')
+    return (date.strftime('%H:%M:%S'))
+
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    f.write(result + '\n')
+
+    f.close()
